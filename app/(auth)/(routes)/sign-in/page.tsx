20367ad9726ff62f08b26d275dd83afcafc4b0c6
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-12-01 11:27:02 +0530

    auth

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,5 +0,0 @@
-const SignInPage = () => {
-  return <div>This is an Unprotected page</div>;
-};
-
-export default SignInPage;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <p>This is a protected Page</p>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
