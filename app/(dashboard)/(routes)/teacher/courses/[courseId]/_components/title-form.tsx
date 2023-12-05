0f7b386f9b22f7a0556e6b4f418559dd91ed3cc6
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-12-05 11:50:58 +0530

    course edit page UI

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+interface TitleFormProps {
+  initialData: {
+    title: string;
+  };
+  courseId: string;
+}
+
+const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+  return <div>TitleForm</div>;
+};
+
+export default TitleForm;
